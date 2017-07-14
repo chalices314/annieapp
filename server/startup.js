526a5f78ead5a7d54421c0cb7c7e86be23471876
cocogirlz2011 <cocogirlz2011@gmail.com>
2017-07-14 14:59:25 -0500

    Hall of fame: deleted html

--- server/startup.js
+++ server/startup.js
@@ -1,22 +1,41 @@
 Meteor.startup(function(){
-  console.log("this is working!");
-  if (Person.find().count()==0) {
-  var a = {
-    name:"Cynthia Breazeal",
-    age:"1967",
-    occupation:"CS Professor",
+    console.log("this is working!");
+    var a = {
+      name:"Cynthia Breazeal",
+      birthdate:"1967",
+      occupation:"Director of the Personal Robots Group at the MIT Media Laboratory",
+      accomplishments:"Pioneer of Social Robotics and Human Robot Interaction"
+
+    };
+    Person.insert(a);
+    var b = {
+      name:"Dr. Marla E. Perez-Davis",
+      birthdate:"n/a",
+      occupation:"NASA Deputy Director",
+      accomplishments:"recipient of NASA Outstanding Leadership Medal"
+
+    };
+    Person.insert(b);
+    var c = {
+      name:"Patricia Bath",
+      birthdate:"1942",
+      occupation:"Ophthalmologist and Inventor",
+      accomplishments:"first African American to complete opthalmology as well as first African American to have medical patent"
+
+    };
+    Person.insert(c);
+   var d = {
+    name:"Limor Fried",
+    birthdate:"n/a",
+    occupation:"Founder/CEO of Adafruit",
+    accomplishments:"Founded Adafuit"
 
   };
-  Person.insert(a);
-
-  var a = {
-    name:"Mia Lipsitz",
-    age:"1997",
-    occupation:"CS Professor",
-
+  Person.insert(d);
+ var e = {
+  name:"Trisha Kothari",
+  birthdate:"1994",
+  occupation:"Product manager/software engineer at Affirm Inc",
+  accomplishments:"Amazing Engineer "
   };
-  Person.insert(a);
-}
-
-
 })
