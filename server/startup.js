005250054e84a9077ec1104184fefce26b01969c
cocogirlz2011 <cocogirlz2011@gmail.com>
2017-07-18 14:06:47 -0500

    halloffame

--- server/startup.js
+++ server/startup.js
@@ -7,7 +7,7 @@ Person.remove({});
       birthdate:"1967",
       occupation:"Director of the Personal Robots Group at the MIT Media Laboratory",
       accomplishments:"Pioneer of Social Robotics and Human Robot Interaction",
-  
+      url: "https://static1.squarespace.com/static/5206a880e4b00fb51870dbf0/t/54500abae4b0d2e64620a8bb/1414531770743/CynthiaBreazeal.jpgc"
     };
     Person.insert(a);
     var b = {
@@ -15,6 +15,7 @@ Person.remove({});
       birthdate:"n/a",
       occupation:"NASA Deputy Director",
       accomplishments:"recipient of NASA Outstanding Leadership Medal",
+      url: "https://www.nasa.gov/sites/default/files/thumbnails/image/perez-davis_hi-res.jpg"
 
     };
     Person.insert(b);
@@ -23,14 +24,15 @@ Person.remove({});
       birthdate:"1942",
       occupation:"Ophthalmologist and Inventor",
       accomplishments:"first African American to complete opthalmology as well as first African American to have medical patent",
-
+      url: "http://blackinventor.com/wp-content/uploads/2012/11/patriciabath07.jpg"
     };
     Person.insert(c);
    var d = {
     name:"Limor Fried",
     birthdate:"n/a",
     occupation:"Founder/CEO of Adafruit",
-    accomplishments:"Founded Adafuit"
+    accomplishments:"Founded Adafuit",
+    url:"http://d2lupdnmi5p5au.cloudfront.net/i__srcd1d6ce6bdcaeabe0cbfe13d02a4c1feb_pare6a224e45bce1ee797181303b75776f0.png"
 
   };
   Person.insert(d);
